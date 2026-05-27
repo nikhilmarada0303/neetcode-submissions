99b72e46a132a666eb8c71e1b08b810f1b05dae6
nikhil_marada <105623800+nikhilmarada0303@users.noreply.github.com>
2026-05-27 10:49:10 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int>pq;
+        for(auto it:stones) pq.push(it);
+        while(pq.size()>1){
+            int x=pq.top(); pq.pop();
+            int y=pq.top(); pq.pop();
+            if(x==y) continue;
+            pq.push(abs(x-y));
+        }if(pq.empty()) return 0;
+        return pq.top();
+    }
+};
