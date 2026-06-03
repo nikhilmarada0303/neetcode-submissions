5ab3e1246dc255cbb66f84bc70868aefb5cd2e81
nikhil_marada <105623800+nikhilmarada0303@users.noreply.github.com>
2026-06-03 10:44:26 +0530

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    bool func(int ind,vector<int>&nums,int n,vector<int>&dp){
+        if(ind>=n) return 1;
+
+        if(dp[ind]!=-1) return dp[ind];
+        bool take=0;
+        for(int i=1;i<=nums[ind];i++){
+            if(func(ind+i,nums,n,dp)) return dp[ind]=1;
+        }
+        return dp[ind]=0;
+    }
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size()-1;
+        vector<int>dp(n+1,-1);
+        return func(0,nums,n,dp);
+    }
+};
